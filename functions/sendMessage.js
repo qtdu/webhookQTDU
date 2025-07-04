dd5e50d079b7d5f673200621d5044baac42b3a16
qtdu <116353460+qtdu@users.noreply.github.com>
2025-07-04 07:24:27 +0700

    Update sendMessage.js

--- functions/sendMessage.js
+++ functions/sendMessage.js
@@ -1,5 +1,5 @@
 export async function handler(event, context) {
-  const WEBHOOK_URL = "https://webhook.site/xxxxxx"; // thay bằng webhook thật
+  const WEBHOOK_URL = "https://oapi.dingtalk.com/robot/send?access_token=67e1b68517f0b043143ad99c3efe152476b965ad45d0899faab686892ff81b07"; // thay bằng webhook thật
 
   const data = {
     content: "🚀 Gửi thành công từ Netlify Function!",
